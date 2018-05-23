b08dc410fcf5210b436ab698ff90a2df6db6b77a
Ji Yim <gsooyim@gmail.com>
2018-05-23 21:52:35 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,4 +17,6 @@ class Pokemon
     pokemon = db.execute("SELECT * FROM pokemon WHERE id=?", id).flatten
     Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], hp: pokemon[3], db: db)
   end
+  
+  def alter_hp(hp)
 end
